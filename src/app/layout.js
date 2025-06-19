a9cda24e3d6ffac1cd3fd3630a5b88a217fe51ed
Tire11 <teejay1177@gmail.com>
2025-06-19 11:27:35 +0100

    Revert "Integrate Clerk authentication with the app"
    This reverts commit 2bee3585f1307bf3ddb20406918bb834214c0e99.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import ThemeCom from '@/components/ThemeCom';
-import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -21,15 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang='en' suppressHydrationWarning>
-        <body>
-          <ThemeCom>
-            <Header />
-            {children}
-          </ThemeCom>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang='en' suppressHydrationWarning>
+      <body>
+        <ThemeCom>
+          <Header />
+          {children}
+        </ThemeCom>
+      </body>
+    </html>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,12 @@
 import Link from 'next/link';
 import DarkModeSwitch from './DarkModeSwitch';
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
       <ul className='flex gap-4'>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
+        <li>
           <Link href={'/sign-in'}>Sign in</Link>
-        </SignedOut>
+        </li>
         <li className='hidden sm:block'>
           <Link href={'/'}>Home</Link>
         </li>
