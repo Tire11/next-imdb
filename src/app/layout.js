2a004c34ed7ccd31a9319ed75e53bffa64ee9535
Tire11 <teejay1177@gmail.com>
2025-06-19 11:27:34 +0100

    Revert "Add SearchBox component and implement search functionality"
    This reverts commit f3f94f78f85919a28883d5db4429e9b88a3df9b4.

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import Header from '@/components/Header';
 import ThemeCom from '@/components/ThemeCom';
 import { ClerkProvider } from '@clerk/nextjs';
 import Navbar from '@/components/Navbar';
-import SearchBox from '@/components/SearchBox';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,7 +28,6 @@ export default function RootLayout({ children }) {
           <ThemeCom>
             <Header />
             <Navbar />
-            <SearchBox />
             {children}
           </ThemeCom>
         </body>
